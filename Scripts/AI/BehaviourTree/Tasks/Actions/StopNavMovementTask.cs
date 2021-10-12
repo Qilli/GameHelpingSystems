632d368eac36554e2604617b2c3f71d9a25c62be
Qilli <39675481+Qilli@users.noreply.github.com>
2021-10-12 13:02:23 +0200

    Update StopNavMovementTask

--- Scripts/AI/BehaviourTree/Tasks/Actions/StopNavMovementTask.cs
+++ Scripts/AI/BehaviourTree/Tasks/Actions/StopNavMovementTask.cs
@@ -7,11 +7,18 @@ namespace Base.AI.Behaviours
     //[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/StopNavMovementTask", order = 1)]
     public class StopNavMovementTask : ActionTreeTask
     {
+        public EditorSharedVariable useNavMeshMovement =
+        new EditorSharedVariable
+        {
+            name = "",
+            type = SharedVariable.SharedType.BOOL
+        };
         public override BehaviourTreeTaskRuntime getRuntimeTask(BehaviourTreeController runtimeController, BehaviourTreeTaskRuntime parent = null)
         {
             StopNavMovementTasRuntime rn = new StopNavMovementTasRuntime();
             rn.type = TaskType.ACTION;
             rn.parent = parent;
+            rn.useNavMeshMovement = runtimeController.Blackboard.getVariableByName(useNavMeshMovement.name) as SharedBool;
             return rn;
         }
 
@@ -20,6 +27,7 @@ namespace Base.AI.Behaviours
         public override List<SerializedProperty> getAllProperties(SerializedObject obj)
         {
             List<SerializedProperty> list = new List<SerializedProperty>();
+            list.Add(obj.FindProperty("useNavMeshMovement"));
             return list;
         }
 
@@ -28,14 +36,18 @@ namespace Base.AI.Behaviours
 
     public class StopNavMovementTasRuntime : ActionTreeTaskRuntime
     {
+        public SharedBool useNavMeshMovement;
         public override TaskResult run(BehaviourTreeController.TreeStatus controller)
         {
             if (hasReadyResult(controller)) return lastResult;
-            //stop nav
-            controller.agent.NavAgent.isStopped = true;
-            //we need to look at our target, force it on instant
-            controller.agent.AgentKinematics.velocity = Vector3.zero;
-            controller.agent.AgentKinematics.rotation = 0;
+            if (useNavMeshMovement.value)
+            {
+                //stop nav
+                controller.agent.NavAgent.isStopped = true;
+                //we need to look at our target, force it on instant
+                controller.agent.AgentKinematics.velocity = Vector3.zero;
+                controller.agent.AgentKinematics.rotation = 0;
+            }
             //cache result
             return lastResult = TaskResult.SUCCESS;
         }
