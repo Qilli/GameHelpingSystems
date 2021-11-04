1ec9b1da60311359e04e4b09ec427acb238788e0
Qilli <39675481+Qilli@users.noreply.github.com>
2021-11-04 12:23:37 +0100

    changes

---
+++ Scripts/CommonInterfaces/IOnUpdate.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Base.CommonInterfaces
+{
+    public interface IOnUpdate
+    {
+        void onUpdate(float delta);
+    }
+}
