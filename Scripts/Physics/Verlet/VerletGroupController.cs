a8329392d0f4deaab53e386b902a4cb3fc860012
Qilli <39675481+Qilli@users.noreply.github.com>
2021-11-08 23:21:07 +0100

    Scene template and verlet base

---
+++ Scripts/Physics/Verlet/VerletGroupController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerletGroupController : MonoBehaviour
+{
+    #region PUBLIC PARAMS
+
+    #endregion
+    #region PRIVATE PARAMS
+
+    #endregion
+
+    #region PUBLIC FUNC
+    #endregion
+    #region PRIVATE FUNC
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
