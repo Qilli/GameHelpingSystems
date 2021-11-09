5fdc70fa48436dc022e9dce02c4e370c3fc1fb0b
Qilli <39675481+Qilli@users.noreply.github.com>
2021-11-09 11:14:57 +0100

    Vertlet controller base

---
+++ EditorScripts/Editor/Physics/Verlet/VerletGroupControllerEditor.cs
@@ -0,0 +1,109 @@
+using UnityEngine;
+using UnityEditor;
+using Base.Physics.Verlet;
+using System;
+
+namespace Base.Editor.Physics.Verlet
+{
+    [CustomEditor(typeof(VerletGroupController))]
+    public class VerletGroupControllerEditor : UnityEditor.Editor
+    {
+        #region PUBLIC PARAMS
+        #endregion
+        #region PRIVATE PARAMS
+        private const int idOffset = 1000;
+        private int selectedPoint = -1;
+        private VerletGroupController localCtrl;
+        private bool leftMouseButtonDown;
+        #endregion
+
+        #region PUBLIC FUNC
+        public void OnSceneGUI()
+        {
+            localCtrl = target as VerletGroupController;
+            checkInput();
+            drawHandles();
+            checkHandleSelection();
+            EditorUtility.SetDirty(localCtrl);
+        }
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+            drawActionButtons();
+            drawSelectedPoint();
+            EditorUtility.SetDirty(localCtrl);
+        }
+
+        #endregion
+        #region PRIVATE FUNC
+        private void drawSelectedPoint()
+        {
+            GUILayout.BeginVertical();
+            if (selectedPoint != -1)
+            {
+                //draw data of point
+                VerletPoint currentPointLocal = localCtrl.getAtIndex(selectedPoint);
+                currentPointLocal.Position=EditorGUILayout.Vector3Field("Position", currentPointLocal.Position);
+                currentPointLocal.PrevPosition= EditorGUILayout.Vector3Field("Prev Position", currentPointLocal.PrevPosition);
+                currentPointLocal.IsLocked = EditorGUILayout.Toggle("Is locked", currentPointLocal.IsLocked);
+            }
+            else GUILayout.Label("No point selected");
+            GUILayout.EndVertical();
+        }
+        private void drawHandles()
+        {
+            for(int a=0;a<localCtrl.PointsCount;++a)
+            {
+                drawSelectedHandle(localCtrl.getAtIndex(a), a);
+            }
+        }
+        private void checkInput()
+        {
+            leftMouseButtonDown = false;
+            if(Event.current.rawType == EventType.MouseDown)
+            {
+                leftMouseButtonDown = true;
+            }
+        }
+        private void checkHandleSelection()
+        {
+            if(leftMouseButtonDown)
+            {
+                if (GUIUtility.hotControl!=0)
+                {
+                    int id = GUIUtility.hotControl - idOffset;
+                    if (localCtrl.isIndexValid(id))
+                    {
+                        selectedPoint = id;
+                    }
+                }
+            }
+        }
+        private void drawActionButtons()
+        {
+            EditorGUILayout.BeginVertical();
+            if(GUILayout.Button("Add new point"))
+            {
+                selectedPoint=localCtrl.addNewPoint();
+            }
+            if (GUILayout.Button("Remove Selected Point"))
+            {
+                if(selectedPoint!=-1)
+                {
+                    localCtrl.removePointAt(selectedPoint);
+                    selectedPoint = -1;
+                }
+            }
+            EditorGUILayout.EndVertical();
+        }
+        private void drawSelectedHandle(VerletPoint current, int id)
+        {       
+            current.Position = Handles.PositionHandle(current.Position, Quaternion.identity);
+            Handles.color = localCtrl.editorPrefs.spheresColor;
+            current.Position = Handles.FreeMoveHandle(id+idOffset,current.Position, Quaternion.identity, localCtrl.editorPrefs.sphereSize, localCtrl.editorPrefs.snap, Handles.SphereHandleCap);
+        }
+        #endregion
+
+    }
+}
--- Scripts/Physics/Verlet/VerletGroupController.cs
+++ Scripts/Physics/Verlet/VerletGroupController.cs
@@ -1,29 +1,70 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class VerletGroupController : MonoBehaviour
+namespace Base.Physics.Verlet
 {
-    #region PUBLIC PARAMS
-
-    #endregion
-    #region PRIVATE PARAMS
-
-    #endregion
-
-    #region PUBLIC FUNC
-    #endregion
-    #region PRIVATE FUNC
-    #endregion
-    // Start is called before the first frame update
-    void Start()
+    public class VerletGroupController : Base.ObjectsControl.BaseObject
     {
-        
-    }
+        #region CLASSES
+        [System.Serializable]
+        public class VerletEditorPrefs
+        {
+            [Header("Defaults")]
+            public Vector3 defaultNewPointPos = -Vector3.up;
+            public Vector3 defaultNewPointPrevPos = Vector3.zero;
+            public Color spheresColor=Color.blue;
+            public float sphereSize=0.2f;
+            public Vector3 snap;
+        }
+        #endregion
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        #region PUBLIC PARAMS
+        [Header("Editor Prefs")]
+        public VerletEditorPrefs editorPrefs;
+        [Header("Simulation settings")]
+        public bool simulationActive = true;
+        #endregion
+        #region PRIVATE PARAMS
+        [SerializeField, HideInInspector]
+        private List<VerletPoint> points = new List<VerletPoint>();
+        #endregion
+
+        #region PUBLIC FUNC
+        public int PointsCount { get => points.Count; }
+        public VerletPoint getAtIndex(int index)
+        {
+            Debug.Assert(index >= 0 && index < PointsCount, "Wrong point index");
+            return points[index];
+        }
+        public bool isIndexValid(int index)
+        {
+            return index >= 0 && index < points.Count;
+        }
+        public int addNewPoint()
+        {
+            VerletPoint p = new VerletPoint();
+            p.Position = editorPrefs.defaultNewPointPos;
+            p.PrevPosition = editorPrefs.defaultNewPointPrevPos;
+            p.IsLocked = false;
+            points.Add(p);
+            return points.Count - 1;
+        }
+
+        public void removePointAt(int selectedPoint)
+        {
+           if(isIndexValid(selectedPoint))
+            {
+                points.RemoveAt(selectedPoint);
+            }
+        }
+
+        public override void onFixedUpdate(float fixedDelta)
+        {
+            base.onFixedUpdate(fixedDelta);
+        }
+        #endregion
+        #region PRIVATE FUNC
+        #endregion
     }
 }
